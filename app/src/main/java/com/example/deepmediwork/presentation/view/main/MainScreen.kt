add33db9e7438c7bdf9a7b647d4e0979bf93205d
HanBI24 <alsltjdrhs@naver.com>
2023-05-20 14:52:22 +0900

    Add to face recog comment in MainScreen

--- app/src/main/java/com/example/deepmediwork/presentation/view/main/MainScreen.kt
+++ app/src/main/java/com/example/deepmediwork/presentation/view/main/MainScreen.kt
@@ -6,21 +6,70 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainScreen() {
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         HomeTopAppBar(title = "홈")
+        RecognizeText()
     }
+}
+
+@Composable
+fun RecognizeText() {
+    Text(
+        modifier = Modifier.padding(top = 52.dp),
+        text = buildAnnotatedString {
+            append("얼굴 인식을 위해\n")
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Red
+                )
+            ) {
+                append("화면을 응시")
+            }
+            append("해 주세요.")
+        },
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+    )
+}
+
+@Composable
+fun RecognizeFinishText() {
+    Text(
+        modifier = Modifier.padding(top = 52.dp),
+        text = buildAnnotatedString {
+            append("얼굴 인식 ")
+            withStyle(
+                style = SpanStyle(
+                    color = Color.Red
+                )
+            ) {
+                append("성공")
+            }
+        },
+        textAlign = TextAlign.Center,
+        style = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+    )
 }
 
 @Composable
