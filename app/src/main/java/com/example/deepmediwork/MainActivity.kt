8641094ecb10b9f9fff20d8377db0328c53678ee
HanBI24 <alsltjdrhs@naver.com>
2023-05-21 12:20:01 +0900

    Add to Camera View

--- app/src/main/java/com/example/deepmediwork/MainActivity.kt
+++ app/src/main/java/com/example/deepmediwork/MainActivity.kt
@@ -3,18 +3,9 @@ package com.example.deepmediwork
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.deepmediwork.presentation.navigation.SetUpNavGraph
-import com.example.deepmediwork.presentation.view.main.MainScreen
 import com.example.deepmediwork.ui.theme.DeepMediWorkTheme
 import dagger.hilt.android.AndroidEntryPoint
 
--- app/src/main/java/com/example/deepmediwork/presentation/view/main/MainScreen.kt
+++ app/src/main/java/com/example/deepmediwork/presentation/view/main/MainScreen.kt
@@ -1,5 +1,17 @@
 package com.example.deepmediwork.presentation.view.main
 
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.FLASH_MODE_ON
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
@@ -7,9 +19,12 @@ import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -18,13 +33,31 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavHostController
 import com.example.deepmediwork.navigation.NavScreen
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MainScreen(
     navController: NavHostController
 ) {
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    )
+
+    if(!permissionState.allPermissionsGranted) {
+        SideEffect {
+            permissionState.launchMultiplePermissionRequest()
+        }
+    }
+
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         HomeTopAppBar()
         RecognizeText()
@@ -99,6 +132,9 @@ fun RecognizeFinishText() {
 
 @Composable
 fun CameraArea() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+
     Box(
         modifier = Modifier
             .padding(32.dp)
@@ -110,7 +146,47 @@ fun CameraArea() {
                 )
             )
     ) {
+        AndroidView(
+            factory = {
+                val previewView = PreviewView(it)
+                showCameraPreview(context, lifecycleOwner, previewView)
+                previewView
+            },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
 
+fun showCameraPreview(
+    context: Context,
+    lifecycleOwner: LifecycleOwner,
+    previewView: PreviewView
+) {
+    val cameraPreview = Preview.Builder().build()
+    val cameraProvider = ProcessCameraProvider.getInstance(context).get()
+    val imageAnalysis = ImageAnalysis.Builder()
+        .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+        .build()
+    val imageCapture = ImageCapture.Builder()
+        .setFlashMode(FLASH_MODE_ON)
+        .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+        .build()
+    val imageSelector = CameraSelector.Builder()
+        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+        .build()
+
+    cameraPreview.setSurfaceProvider(previewView.surfaceProvider)
+    try {
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            imageSelector,
+            cameraPreview,
+            imageAnalysis,
+            imageCapture
+        )
+    } catch(e: Exception) {
+        e.printStackTrace()
     }
 }
 
